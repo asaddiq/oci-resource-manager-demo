e06b56560593917519b3822bb38295136191cc61
Arif Saddiq <arif.saddiq@oracle.com>
2024-05-09 13:26:34 +0100

    update

--- identity.tf
+++
@@ -1,9 +0,0 @@
-
-# ------ Root Compartment
-locals {
-    DeploymentCompartment_id              = var.compartment_ocid
-}
-
-output "DeploymentCompartmentId" {
-    value = local.DeploymentCompartment_id
-}
